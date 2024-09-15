47a8ddaa5ec0fbcf0f5ef7ff149808237b54c6b6
amimanye <amimanyeaglago@gmail.com>
2024-09-15 12:44:50 +0000

    remove trailing slash from origin in cors

--- server/index.js
+++ server/index.js
@@ -13,7 +13,7 @@ const PORT = process.env.PORT || 3000;
 // CORS configuration
 app.use(
   cors({
-    origin: "https://cryptotrade-phi.vercel.app/",
+    origin: "https://cryptotrade-phi.vercel.app",
     methods: ["GET"],
     allowedHeaders: ["Content-Type", "Authorization"],
   })
